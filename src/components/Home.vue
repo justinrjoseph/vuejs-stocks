4063ea758ff3bd001e16fa027edf964de3e7e273
Justin Joseph <justin.joseph0@yahoo.com>
2017-02-07 20:47:42 -0500

    Initial commit

---
+++ src/components/Home.vue
@@ -0,0 +1,22 @@
+<template lang="html">
+  <div>
+    <h1>Manage your Portfolio</h1>
+    <h4>Save &amp; Load your Data</h4>
+    <h6>Use <strong>End Day</strong> to begin a New Day!</h6>
+    <hr>
+    <p>Funds: {{ funds | currency }}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    computed: {
+      funds() {
+        return this.$store.getters.funds;
+      }
+    }
+  }
+</script>
+
+<style lang="css">
+</style>
---
+++ src/components/portfolio/Stock.vue
@@ -0,0 +1,67 @@
+<template lang="html">
+  <div class="col-sm-6 col-md-4">
+    <div class="panel panel-info">
+      <div class="panel-heading">
+        <h3 class="panel-title">{{ stock.name }} <small>(Price: {{ stock.price }}) | Quantity: {{ stock.quantity }}</small></h3>
+      </div>
+      <div class="panel-body">
+        <div class="pull-left">
+          <input
+            type="number"
+            class="form-control"
+            :class="{ danger: invalidQuantity }"
+            placeholder="Quantity"
+            v-model="quantity">
+        </div>
+        <div class="pull-right">
+          <button
+            class="btn"
+            :class="{ 'btn-danger': invalidQuantity, 'btn-success': !invalidQuantity }"
+            @click="sellStock()"
+            :disabled="invalidQuantity || quantity <= 0 || !Number.isInteger(quantity)">
+            {{ invalidQuantity ? 'Exceeds Portfolio' : 'Sell' }}
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import { mapActions } from 'vuex';
+
+  export default {
+    props: ['stock'],
+    data() {
+      return {
+        quantity: 0
+      };
+    },
+    computed: {
+      invalidQuantity() {
+        return this.quantity > this.stock.quantity;
+      }
+    },
+    methods: {
+      ...mapActions({
+        processSale: 'sellStock'
+      }),
+      sellStock() {
+        const order = {
+          stockId: this.stock.id,
+          stockPrice: this.stock.price,
+          quantity: this.quantity
+        };
+
+        this.processSale(order);
+        this.quantity = 0;
+      }
+    }
+  }
+</script>
+
+<style lang="css">
+  .danger {
+    border: 1px solid red;
+  }
+</style>
